0de662b37ea5c27c670ac4d6aa52e3efb1df078b
ChadRM <chad.r.miller@gmail.com>
2020-05-18 19:57:35 +0000

    Updated stuff

--- get_files.sh
+++ get_files.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 rm inventory/*
-scp chad@10.9.185.126:/home/chad/mesos-edu2/education-toolbox/envctl/student_key .
+scp chad@10.9.185.126:/home/chad/meso-edu3/education-toolbox/education-toolbox/envctl/student_key .
 chmod 600 student_key
-scp chad@10.9.185.126:/home/chad/mesos-edu2/education-toolbox/envctl/inventory/dist/* ./inventory/
+scp chad@10.9.185.126:/home/chad/meso-edu3/education-toolbox/education-toolbox/envctl/inventory/dist/* ./inventory/
 cat ./inventory/* > ./inventory/inv.concat
 #rm ./inventory/dc*.txt
 #parse this shit in python
--- prep-cka.sh
+++ prep-cka.sh
@@ -8,6 +8,8 @@ echo "========================Install K9s Client..."
 ansible-playbook --key-file=student_key -i ./inventory/inv.yaml install-k9s.yaml
 echo "========================Upload Sock Shop..."
 ansible-playbook --key-file=student_key -i ./inventory/inv.yaml upload-sockshop.yaml
+echo "========================Install Nano..."
+ansible-playbook --key-file=student_key -k ./inventory/inv.yaml install-nano.yaml
 echo "========================Set up Instructor Cluster..."
 ansible-playbook --key-file=student_key -i ./inventory/inv.yaml install-k8s.yaml -l dcos-training-1
 
